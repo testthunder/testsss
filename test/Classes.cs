6755ea0e93fc70e2d3256c22e04df20bdac741fd
testthunder <30811662+testthunder@users.noreply.github.com>
2017-08-07 14:30:14 -0700

    Master

---
+++ test/Classes.cs
@@ -0,0 +1,48 @@
+/*
+ *  C# Program to Demonstrate Properties of the Class
+ */
+using System;
+class Student
+{
+    private string myName = "N/A";
+    private int myAge = 0;
+    public string Name
+    {
+        get
+        {
+            return myName;
+        }
+        set
+        {
+            myName = value;
+        }
+    }
+    public int Age
+    {
+        get
+        {
+            return myAge;
+        }
+        set
+        {
+            myAge = value;
+        }
+    }
+ 
+    public override string ToString()
+    {
+        return "Name = " + Name + ", Age = " + Age;
+    }
+ 
+    public static void Main()
+    {
+        Student Student = new Student();
+        Console.WriteLine("Student details - {0}", Student);
+        Student.Name = "BOB";
+        Student.Age = 49;
+        Console.WriteLine("Student details - {0}", Student);
+        Student.Age += 5;
+        Console.WriteLine("Student details - {0}", Student);
+        Console.ReadLine();
+    }
+}
